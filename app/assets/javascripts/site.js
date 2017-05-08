a1e1693548ffb4269756e1a65a724617edf7fcca
Jake Hill <Jake_hill666@hotmail.co.uk>
2017-05-08 14:02:12 +0200

    site.js turbolinks

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).on("turbolinks:load", function(){
 	console.log("Hey! from site.js");
 	$(".rating").raty( { path: '/assets', scoreName: 'comment[rating]' });
 });
