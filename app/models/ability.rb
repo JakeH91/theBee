58325ba3134054775c4e1ae538f5bb4271f1f17c
Jake Hill <Jake_hill666@hotmail.co.uk>
2017-05-10 12:28:01 +0200

    allow only admin to delete comments

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,6 +3,9 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id 
+    can :manage, User, id: user.id
+    if !user.admin?
+    	cannot :destroy, Comment
+    end
   end
 end
