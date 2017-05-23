1f02c69c4e1f78c329acfc8e765771fdcf9bed61
Jake Hill <Jake_hill666@hotmail.co.uk>
2017-05-23 13:19:06 +0200

    create two unit tests for Product model, 1 to test average_rating, 1 to check validity of product with no name

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+describe Product do
+	context "when the product has comments" do
+		before do
+			@product = Product.create!(name: "Race Bike")
+			@user = User.create!(first_name: "John", last_name: "Heggs", email: "johnheggschodycone@johnheggs.chodycone", password: "johnheggs")
+			@product.comments.create!(rating: 1, user: @user, body: "Bloody awful bike!")
+			@product.comments.create!(rating: 3, user: @user, body: "Yeah it's not too bad, all things considered")
+			@product.comments.create!(rating: 5, user: @user, body: "Myyyy goodness I love this thing!")
+		end
+
+		it "returns average rating of all comments" do
+			expect(@product.average_rating).to eq 3
+		end
+
+		it "is not valid" do
+			expect(Product.new(description: "Very lovely little hat")).not_to be_valid
+		end
+	end
+end
