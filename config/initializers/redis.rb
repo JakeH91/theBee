b54342aff90532dd3495700e58fb75b32928c96b
Jake Hill <Jake_hill666@hotmail.co.uk>
2017-06-05 15:56:15 +0200

    add conditional in config/intiializers/session_store.rb

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,8 +1,5 @@
-# if Rails.env.production?
-# 	$redis = Redis.new(url: ENV["REDIS_URL"])
-# elsif Rails.env.development?
-# 	$redis = Redis.new(:host => 'localhost', :port => 6379)
-# end
-
-uri = URI.parse(ENV["REDIS_URL"] || "redis://localhost:6379/" )
-$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+if Rails.env.production?
+	$redis = Redis.new(url: ENV["REDIS_URL"])
+elsif Rails.env.development?
+	$redis = Redis.new(:host => 'localhost', :port => 6379)
+end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,6 @@
 
 Rails.application.config.session_store :cookie_store, key: '_nameofapp_session'
 
-Rails.application.config.session_store :redis_store, servers: ["redis://localhost:6379/0/session"]
+if Rails.env.development?
+	Rails.application.config.session_store :redis_store, servers: ["redis://localhost:6379/0/session"]
+end
