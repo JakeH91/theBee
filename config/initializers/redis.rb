e1bbe52656cac9e238c1c8526c5032186daf2c98
Jake Hill <Jake_hill666@hotmail.co.uk>
2017-06-05 15:09:05 +0200

    try parsing REDIS_URL

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,8 @@
-if Rails.env.production?
-	$redis = Redis.new(url: ENV["REDIS_URL"])
-elsif Rails.env.development?
-	$redis = Redis.new(:host => 'localhost', :port => 6379)
-end
+# if Rails.env.production?
+# 	$redis = Redis.new(url: ENV["REDIS_URL"])
+# elsif Rails.env.development?
+# 	$redis = Redis.new(:host => 'localhost', :port => 6379)
+# end
+
+uri = URI.parse(ENV["REDIS_URL"] || "redis://localhost:6379/" )
+$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
